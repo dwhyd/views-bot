fa22f51af1d81da63d53d2869f556f5dc3d5c3c4
Diki Wahyudi <68983872+dwhyd@users.noreply.github.com>
2022-07-07 07:53:03 +0700

    Update bot-views.py

--- bot-views.py
+++ bot-views.py
@@ -9,13 +9,12 @@ from useragent import header
 proxyPort=9050
 ctrlPort=9051
 referer= ["https://ask.com", "https://mobilelegends.com", "https://www.nasa.gov", "https://deepmind.com", "https://tokopedia.com", "https://bukalapak.com", "https://lazada.com", "https://alibaba.com", "https://www.dpr.go.id", "https://www.mpr.go.id"]
-# site = input("ketikkan alamat blog (misal: http://link.com) : ")
+site = input("ketikkan alamat blog (misal: http://link.com) : ")
 jumlah = input("jumlah viewnya  : ")
 jumlah =int(jumlah)
-blogger= "https://pteridophytadev.blogspot.com/2022/05/cara-menghubungkan-2-vlan-berbeda.html"
 def run(i):
      tr.reset_identity()
-     response = tr.get(blogger, headers={"User-Agent": random.choice(header),"Referer":random.choice(referer) })
+     response = tr.get(site, headers={"User-Agent": random.choice(header),"Referer":random.choice(referer) })
      print("View +", (i+1))
 
 if __name__ == '__main__':
